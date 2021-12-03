b17ed1c30686c4571122cb8ef9ed265233dfb236
NaFaPaz <nfanego@gmail.com>
2021-12-03 14:56:57 -0300

    add index, style and script

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const header = document.querySelector('header');
+const themeButton = document.querySelector('.theme-Button');
+const navBar = document.querySelectorAll('.nav-bar');
+const footer = document.querySelector('footer');
+
+function handleToggle (event){
+    if (themeButton.checked === true) {
+        navBar.forEach((nB)=>{nB.style.backgroundColor = 'var(--dark-nav-back)'});
+        header.style.backgroundColor = 'var(--dark-back)';
+        footer.style.backgroundColor = 'var(--dark-back)';
+    } else {
+        navBar.forEach((nB)=>{nB.style.backgroundColor = 'var(--nav-back)'});
+        header.style.backgroundColor = 'var(--back)';
+        footer.style.backgroundColor = 'var(--back)';
+    }
+};
+
+themeButton.addEventListener('click',handleToggle);

0b0d4d01af4a81a17cecfa944ceca505bbbf889f
NaFaPaz <nfanego@gmail.com>
2021-12-06 10:42:47 -0300

    add theme button

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ const header = document.querySelector('header');
 const themeButton = document.querySelector('.theme-Button');
 const navBar = document.querySelectorAll('.nav-bar');
 const footer = document.querySelector('footer');
+const label = document.querySelector('label');
 
 function handleToggle (){
     if (themeButton.checked === true) {
         navBar.forEach((nB)=>{nB.style.backgroundColor = 'var(--dark-nav-back)'});
         header.style.backgroundColor = 'var(--dark-back)';
         footer.style.backgroundColor = 'var(--dark-back)';
+        label.style.color = 'white';
     } else {
         navBar.forEach((nB)=>{nB.style.backgroundColor = 'var(--nav-back)'});
         header.style.backgroundColor = 'var(--back)';
         footer.style.backgroundColor = 'var(--back)';
+        label.style.color = 'black';
     }
 };
 

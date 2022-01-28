68a5efcbd94ce427523860b34cd712e2f063066c
NaFaPaz <nfanego@gmail.com>
2022-01-27 22:22:18 -0300

    add section expand

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const footer = document.querySelector("footer");
 const label = document.querySelector("label");
 const menuBtn = document.querySelectorAll(".nav-link");
 const hamb = document.querySelector(".hamb");
+const acordeon = document.querySelectorAll(".expand");
 
 function handleBg() {
   if (themeButton.checked === true) {
@@ -26,10 +27,12 @@ function handleMenu() {
   if (navBar.style.display === "flex") {
     navBar.style.display = "none";
     header.style.height = 0;
+    /* header.style.pointerEvents = "none"; */
     document.documentElement.style.setProperty("--hamb", "white");
   } else {
     navBar.style.display = "flex";
     header.style.height = "100vh";
+    /* header.style.pointerEvents = "auto"; */
     if (themeButton.checked === true) {
       document.documentElement.style.setProperty("--hamb", "white");
     } else {
@@ -38,8 +41,24 @@ function handleMenu() {
   }
 }
 
+function handleExpand(event) {
+  const children = event.currentTarget.children;
+  event.currentTarget.classList.toggle("expand");
+  if (event.currentTarget.classList.contains("expand")) {
+    for (let i = 1; i < children.length; i++) {
+      children[i].style.display = "none";
+    }
+  } else {
+    for (let i = 1; i < children.length; i++) {
+      children[i].style.display = "block";
+    }
+  }
+}
+
 menuBtn.forEach((btn) => {
   btn.addEventListener("click", handleMenu);
 });
-
 themeButton.addEventListener("click", handleBg);
+acordeon.forEach((item) => {
+  item.addEventListener("click", handleExpand);
+});
